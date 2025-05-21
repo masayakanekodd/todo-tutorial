1ce1c7380fc90114d9311a4cae28c37700589b4d
masayakanekodd <your.github@email.com>
2025-05-21 21:41:04 +0900

    Fix ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,16 +9,14 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+export default [
+  {
+    ignores: [".eslintrc.js"],
+  },
+  ...compat.extends("next/core-web-vitals"),
   {
     rules: {
-      '@typescript-eslint/no-unused-vars': 'off',
+      "@typescript-eslint/no-unused-vars": "off",
     },
   },
-);
+];
